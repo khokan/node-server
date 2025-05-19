2841024a1c0b8a9581053dea94e284ec20e4639a
khokan77 <shossain.khokon@gmail.com>
2025-05-19 06:17:04 +0600

    CRUD updated

--- coffee-monster.js
+++ coffee-monster.js
@@ -1,40 +1,39 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express(); // create express app
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 app.use(cors()); // for cross-origin
 app.use(express.json()); // for parsing application/json
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`);
-  });
+  console.log(`server listening on port ${port}`);
+});
 
-  app.get("/", (req, res) => {
-    res.send("Hello world from coffee-monster server ..");
-  });
+app.get("/", (req, res) => {
+  res.send("Hello world from coffee-monster server ..");
+});
 
-
-const uri =`mongodb+srv://${process.env.DB_USER_COFFEE}:${process.env.DB_PASS_COFFEE}@cluster0.vqav3xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER_COFFEE}:${process.env.DB_PASS_COFFEE}@cluster0.vqav3xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri,  {
+const client = new MongoClient(uri, {
   serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-  }
-}
-);
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    const coffesCollection = client.db('coffeDB').collection('coffees')
+    const coffesCollection = client.db("coffeDB").collection("coffees");
+    const usersCollection = client.db("coffeDB").collection("users");
 
     app.get("/coffees", async (req, res) => {
       const result = await coffesCollection.find().toArray();
@@ -45,28 +44,27 @@ async function run() {
       const id = req.params.id;
 
       if (!ObjectId.isValid(id)) {
-        return res.status(400).json({ error: 'Invalid coffee ID format' });
+        return res.status(400).json({ error: "Invalid coffee ID format" });
       }
-    
+
       const query = { _id: new ObjectId(id) };
       const result = await coffesCollection.findOne(query);
       res.send(result);
     });
 
+    app.post("/coffees", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffesCollection.insertOne(newCoffee);
+      res.send(result);
+    });
 
-    app.post('/coffees', async (req, res) => {
-        const newCoffee = req.body
-        const result = await coffesCollection.insertOne(newCoffee)
-        res.send(result)
-    })
-   
     app.put("/coffees/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upset: true };
       const updatedCoffee = req.body;
       const updatedDoc = {
-        $set: updatedCoffee        
+        $set: updatedCoffee,
       };
       const result = await coffesCollection.updateOne(
         filter,
@@ -83,11 +81,22 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
