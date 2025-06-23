392f1d7dacf038e0947b7ab03dc5c3dd23b34f73
khokan77 <shossain.khokan@gmail.com>
2025-06-23 16:27:54 +0600

    port updated

--- plant-net-conceptual.js
+++ plant-net-conceptual.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const app = express()
 // middleware
 const corsOptions = {
